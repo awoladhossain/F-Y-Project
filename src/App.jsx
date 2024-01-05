9e99ed202a78eba0ca51f1a83b6e7faf7a071ea5
Awolad Hossain <awoladh04@gmail.com>
2024-01-05 11:39:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello this is awolad</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+};
+
+export default App;
